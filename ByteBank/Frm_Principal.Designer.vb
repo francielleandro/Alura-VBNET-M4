a31cc4d7d54b83d3e8e5547dec45e9305a481f07
Franciel Leandro <francielleandro@gmail.com>
2021-02-26 02:42:26 -0300

    criando teste de divisão com if

--- ByteBank/Frm_Principal.Designer.vb
+++ ByteBank/Frm_Principal.Designer.vb
@@ -26,6 +26,10 @@ Partial Class Frm_Principal
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.Aula01ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.Vídeo01ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Vídeo02ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Vídeo03ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Txt_Denominador = New System.Windows.Forms.TextBox()
+        Me.Lbl_denominador = New System.Windows.Forms.Label()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -49,7 +53,7 @@ Partial Class Frm_Principal
         '
         'Aula01ToolStripMenuItem
         '
-        Me.Aula01ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Vídeo01ToolStripMenuItem})
+        Me.Aula01ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Vídeo01ToolStripMenuItem, Me.Vídeo02ToolStripMenuItem, Me.Vídeo03ToolStripMenuItem})
         Me.Aula01ToolStripMenuItem.Name = "Aula01ToolStripMenuItem"
         Me.Aula01ToolStripMenuItem.Size = New System.Drawing.Size(58, 20)
         Me.Aula01ToolStripMenuItem.Text = "Aula 01"
@@ -60,11 +64,41 @@ Partial Class Frm_Principal
         Me.Vídeo01ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
         Me.Vídeo01ToolStripMenuItem.Text = "Vídeo 01"
         '
+        'Vídeo02ToolStripMenuItem
+        '
+        Me.Vídeo02ToolStripMenuItem.Name = "Vídeo02ToolStripMenuItem"
+        Me.Vídeo02ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.Vídeo02ToolStripMenuItem.Text = "Vídeo 02"
+        '
+        'Vídeo03ToolStripMenuItem
+        '
+        Me.Vídeo03ToolStripMenuItem.Name = "Vídeo03ToolStripMenuItem"
+        Me.Vídeo03ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.Vídeo03ToolStripMenuItem.Text = "Vídeo 03"
+        '
+        'Txt_Denominador
+        '
+        Me.Txt_Denominador.Location = New System.Drawing.Point(15, 104)
+        Me.Txt_Denominador.Name = "Txt_Denominador"
+        Me.Txt_Denominador.Size = New System.Drawing.Size(100, 20)
+        Me.Txt_Denominador.TabIndex = 2
+        '
+        'Lbl_denominador
+        '
+        Me.Lbl_denominador.AutoSize = True
+        Me.Lbl_denominador.Location = New System.Drawing.Point(12, 88)
+        Me.Lbl_denominador.Name = "Lbl_denominador"
+        Me.Lbl_denominador.Size = New System.Drawing.Size(39, 13)
+        Me.Lbl_denominador.TabIndex = 3
+        Me.Lbl_denominador.Text = "Label1"
+        '
         'Frm_Principal
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(728, 412)
+        Me.Controls.Add(Me.Lbl_denominador)
+        Me.Controls.Add(Me.Txt_Denominador)
         Me.Controls.Add(Me.Lbl_Principal)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
@@ -82,4 +116,8 @@ Partial Class Frm_Principal
     Friend WithEvents MenuStrip1 As MenuStrip
     Friend WithEvents Aula01ToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents Vídeo01ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Vídeo02ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Vídeo03ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Txt_Denominador As TextBox
+    Friend WithEvents Lbl_denominador As Label
 End Class
--- ByteBank/Frm_Principal.vb
+++ ByteBank/Frm_Principal.vb
@@ -9,11 +9,52 @@ Public Class Frm_Principal
         ' Add any initialization after the InitializeComponent() call.
         Me.Text = "Projeto ByteBank"
         Lbl_Principal.Text = "Projeto ByteBank"
+        Lbl_denominador.Text = "Número denominador"
     End Sub
 
     Private Sub Vídeo01ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Vídeo01ToolStripMenuItem.Click
         Dim conta As New ContaCorrente(4009, 481235)
         MsgBox("o número de contas correntes criadas são de " + ContaCorrente.TotalDeContasCriadas.ToString)
         MsgBox("o valor total das taxas de operação é de " + ContaCorrente.TaxaOperacao.ToString)
+    End Sub
+
+    Private Sub Vídeo02ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Vídeo02ToolStripMenuItem.Click
+        Dim denominador As Integer = Txt_Denominador.Text
+        Dim resposta As Integer = TestarDivisao(denominador)
+
+
+        If resposta = -1 Then
+            MsgBox("Não é possivel fazer a divisão porque o denominador é igual a zero")
+        ElseIf resposta = -2 Then
+            MsgBox("Não é possivel fazer ad divisão porque o denominador é maior que o numerador")
+        Else
+            MsgBox("O valor da divisão entre 10 e " + denominador.ToString + " é de " + resposta.ToString)
+        End If
+    End Sub
+
+    Function TestarDivisao(Valor As Integer) As Integer
+        Dim Resultado As Integer = EfetuarDivisao(10, Valor)
+
+        If Resultado = -1 Then
+            Return -1
+        ElseIf Resultado = -2 Then
+            Return -2
+        End If
+        Return Resultado
+    End Function
+
+    Function EfetuarDivisao(numerador As Integer, denominador As Integer) As Integer
+
+        If denominador = 0 Then
+            Return -1
+        ElseIf denominador > numerador Then
+            Return -2
+        End If
+
+        Return numerador / denominador
+    End Function
+
+    Private Sub Vídeo03ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Vídeo03ToolStripMenuItem.Click
+
     End Sub
 End Class
